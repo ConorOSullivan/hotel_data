2665c3c5a91307f5dc8e6cb677694b5d5557c389
Conor O'Sullivan <conorosullivan@Conors-MacBook-Pro.local>
2015-06-13 18:32:38 -0700

    adding create sf hotels statement

---
+++ sf_hotels.sql
@@ -0,0 +1,12 @@
+create table sf_hotels
+(
+	name varchar,
+	zipcode varchar,
+	country varchar,
+	rating float,
+	highrate float,
+	lowrate float,
+	description varchar,
+	confidence bigint,
+	city varchar
+);
